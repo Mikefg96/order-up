75386b7b8cbada1140fa30225f42f9522d509d78
Miguel Flores <mike_fg96@hotmail.com>
2020-11-02 14:21:19 -0700

    Se implementa router de React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import LandingScreen from './screens/LandingScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import LoginScreen from './screens/LoginScreen'
+
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
-  );
+		<Router>
+			<Header/>
+			<Container>
+				<main className='py-3'>
+					<Route path='/' component={LandingScreen} exact></Route>
+					<Route path='/register' component={RegisterScreen}></Route>
+					<Route path='/login' component={LoginScreen}></Route>
+				</main>
+			</Container>
+			<Footer/>
+		</Router>
+	);
 }
 
 export default App;
